5dfe48d38528a977f6c22270ce2162c57fbad5f6
Wilder <saske21x@gmail.com>
2022-07-01 14:58:17 -0500

    Uso de console

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // console.log('Hola mundo')
 let a = 10;
-let b = 10;
-let c = 10;
+let b = 20;
+let c = 30;
 let d = 10;
-let x = a + b;
+let x = a + b + c;
 
-
+console.log('a' , a)
+console.log('b' , b)
+console.log('c' , c)

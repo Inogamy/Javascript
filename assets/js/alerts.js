3c8b7f75159ae585f8b49bfd42e182b0063c0c0a
Wilder <saske21x@gmail.com>
2022-07-04 15:24:01 -0500

    Seccion 2 JS

---
+++ assets/js/alerts.js
@@ -0,0 +1,8 @@
+// alert('Hola mundo');
+
+// let nombre = prompt('¿;cual es tu nombre?', 'Sin nombre')
+// console.log( nombre );
+// console.log('****' + nombre + '*****')
+
+const seleccion = confirm('¿Estas seguro de borrar esto');
+console.log( seleccion )

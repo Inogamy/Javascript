d3ac25ac83419ea689c8f223a900f1e1c70c9138
Inogamy <saske21x@gmail.com>
2022-07-04 20:37:25 -0500

    Arreglos

---
+++ assets/js/arreglos.js
@@ -0,0 +1,28 @@
+//   const arr = new Array(10);
+//  const arr = [];
+//   console.log(arr);
+
+let videoJuego = [ 'Mario 3', 'Megaman', 'Chrono Trigger'];
+
+console.log({videoJuego});
+
+console.log(videoJuego[0]);
+
+let arregloCosas = [
+    true,
+    123,
+    'Fernando',
+    1 + 2,
+    function(){},
+    ()=>{},
+    { a: 1},
+    ['x', 'Megaman', 'Zero', 'Dr.ligth', [
+        'Dr. Willy',
+        'Woodman'
+    ]]
+]
+
+// console.log({arregloCosas});
+// console.log(arregloCosas[2]);
+
+console.log(arregloCosas[7][4][1]);

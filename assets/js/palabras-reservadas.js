c168d7328869fd50c718b333abd3434963072032
Inogamy <saske21x@gmail.com>
2022-07-04 19:54:34 -0500

    17-21 seccion 3

---
+++ assets/js/palabras-reservadas.js
@@ -0,0 +1,19 @@
+
+
+let constante = 123;
+
+// const
+// if
+// try
+// while
+// for()
+// enum
+
+let objeto$ = 123;
+let precio_99_99 = 123;
+
+let jugadorConPuntajeMasAlto = 'John' // camel case para variables o asignar valores
+
+class Juegoanio{ // uppercase para clases
+
+}
---
+++ assets/js/primitivos.js
@@ -0,0 +1,35 @@
+
+
+let nombre = 'peter parker';
+console.log(nombre);
+
+nombre = 'Ben parker';
+console.log(nombre);
+
+nombre = "Tia May";
+nombre = `Tia May`;
+
+console.log( typeof nombre);
+
+nombre = 123;
+console.log(nombre);
+
+let esMArvel = false;
+console.log(typeof esMArvel);
+
+let edad = 33;
+console.log(typeof edad);
+
+edad = 33.0001
+console.log(typeof edad);
+
+let superPoder;
+console.log(typeof superPoder);
+
+let soyNull = null;
+console.log(typeof soyNull);
+
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+
+console.log(symbol1 === symbol2);

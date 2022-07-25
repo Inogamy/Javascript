6ff969c872d6340ec5f45a66c16e9e433781573c
Inogamy <saske21x@gmail.com>
2022-07-24 20:43:57 -0500

    Seccion 08 Cap 90

--- 03-webpack-inicial/webpack.config.js
+++ 03-webpack-inicial/webpack.config.js
@@ -1,12 +1,36 @@
+const HtmlWebpack = require('html-webpack-plugin')
+
 
 module.exports = {
     mode: 'development',
 
+    output: {
+        clean: true //limpia los html
+    },
+
     module: {
-        rule: []
+        rules: [
+            {
+                test: /\.html$/,// busca a cualquier archivo html hace el requerimiento
+                loader: 'html-loader',
+                options: {
+                    sources: false
+                }
+            },
+            {
+                test: /\.css$/,// busca cualquier archivo css hace el requermientos
+                use: [ 'style-loader', 'css-loader']
+            }
+        ]
     },
     optimization: {},
 
 
-    plugin: [],
+    plugins: [
+       new HtmlWebpack({ // instancia el htmlwebpack y modifica al main
+            title: 'Mi Webpack app',
+            // filename: 'index.html',
+            template: './src/index.html'
+       })
+    ],
 }

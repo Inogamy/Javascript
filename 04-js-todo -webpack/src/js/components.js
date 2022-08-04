2b1dc7d7b6c04ce1e12124c2c9ae1c9c81a2aeee
Wilder <saske21x@gmail.com>
2022-08-04 15:41:27 -0500

    Sección 10 cap 112

--- 04-js-todo -webpack/src/js/components.js
+++ 04-js-todo -webpack/src/js/components.js
@@ -1,10 +1,11 @@
-import { Todo } from '../classes';
+import { todoList } from '..';
+import { Todo, TodoList } from '../classes';
 // Referencias al HTML 
 const divTodoList = document.querySelector('.todo-list');
 const txtInput    = document.querySelector('.new-todo')
 
 
-export const crearTodoHtml = ( todo ) =>{
+export const crearTodoHtml = ( todo ) =>{//insertar el nuevo todo al html
 
     const htmlTodo = `
     <li class="${ (todo.completado)? 'completed' : ''}" data-id="${ todo.id }">
@@ -27,8 +28,30 @@ export const crearTodoHtml = ( todo ) =>{
 
 // Eventos
 txtInput.addEventListener( 'keyup', (event) => {
-    if ( event.keyCode === 13 && txtInput.value.length > 0){
+    if ( event.keyCode === 13 && txtInput.value.length > 0){//insertar una nueva tarea
         console.log(txtInput.value);
-        const nuevoTodo = new Todo( txtInput.value )
+        const nuevoTodo = new Todo( txtInput.value );
+        todoList.nuevoTodo(nuevoTodo);
+
+        crearTodoHtml( nuevoTodo );
+        txtInput.value = "";
     }
+});
+
+divTodoList.addEventListener('click', (event)=>{
+
+    const nombreElemento = event.target.localName// input, label o button
+    const todoElemento   = event.target.parentElement.parentElement// Referencia al li
+    const todoId         = todoElemento.getAttribute('data-id')// Obtiene el id del todoElemnto
+
+    if ( nombreElemento.includes('input')){// Click en el check
+
+        todoList.marcarCompletado ( todoId );//Para hacer el check
+        todoElemento.classList.toggle( 'completed' );//Para hacer el tachado a la tarea
+    } else if ( nombreElemento.includes('button')){
+        todoList.eliminarTodo( todoId );// Elimina del arreglo
+        divTodoList.removeChild( todoElemento )// Eliminar del HTML
+    }
+
+    console.log(todoList);
 })
